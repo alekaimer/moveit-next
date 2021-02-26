d0d76467d17379a8264fe1dcea1021820ed707e7
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-26 16:57:13 -0300

    add Countdown component, title of page and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdows";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
+
+
       <ExperienceBar />
       
       <section>
         <div className={styles.leftContainer}>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div className={styles.rightContainer}>
 
