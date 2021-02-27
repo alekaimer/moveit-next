a229ded480d6a0cebc58fba5f9088334c92f6c0a
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-27 19:17:09 -0300

    add level in the Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ChallengesContexts } from '../contexts/ChallengeContexts'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContexts)
   return (
     <div className={styles.container}>
       <img src="https://github.com/alekaimer.png" alt="Alexandre Kaimer" />
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Alexandre Kaimer</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
