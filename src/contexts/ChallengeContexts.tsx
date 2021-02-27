f00bb89125e3d982c24786d02c67ec11cd2daac9
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-27 12:25:22 -0300

    add new data to the context

--- src/contexts/ChallengeContexts.tsx
+++ src/contexts/ChallengeContexts.tsx
@@ -8,13 +8,15 @@ interface ChallengesProviderProps {
 
 export function ChallengesProvider( {children} : ChallengesProviderProps ) {
   const [level, setLevel] = useState(1)
+  const {currentExperience, setCurrentExperience} = useState(0)
+  const [challengesCompleted, setChallengesCompleted] = useState(0)
 
   function levelUp() {
     setLevel(level + 1)
   }
 
   return (
-    <challengesContexts.Provider value={{level, levelUp}}>
+    <challengesContexts.Provider value={{level, levelUp, currentExperience, challengesCompleted}}>
       {children}
     </challengesContexts.Provider>
   )
