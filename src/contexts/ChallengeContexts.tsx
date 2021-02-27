6f97b6fa8617272794a2604b82616583043e599d
Alexandre Kaimer <alekaimer@gmail.com>
2021-02-27 12:21:46 -0300

    rearranging contexts

--- src/contexts/ChallengeContexts.tsx
+++ src/contexts/ChallengeContexts.tsx
@@ -1,3 +1,21 @@
-import { createContext } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 export const challengesContexts = createContext({})
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export function ChallengesProvider( {children} : ChallengesProviderProps ) {
+  const [level, setLevel] = useState(1)
+
+  function levelUp() {
+    setLevel(level + 1)
+  }
+
+  return (
+    <challengesContexts.Provider value={{level, levelUp}}>
+      {children}
+    </challengesContexts.Provider>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,12 @@
 import '../styles/global.css'
 import '../contexts/ChallengeContexts'
-import { challengesContexts } from '../contexts/ChallengeContexts'
-import { useState } from 'react'
+import { ChallengesProvider } from '../contexts/ChallengeContexts'
 
 function MyApp({ Component, pageProps }) {
-  const [level, setLevel] = useState(1)
-
-  function levelUp() {
-    setLevel(level + 1)
-  }
-
   return (
-    <challengesContexts.Provider value={{level, levelUp}}>
+    <ChallengesProvider>
       <Component {...pageProps} />)
-    </challengesContexts.Provider>
+    </ChallengesProvider>
   )
 }
 
