b04a848bc2112424e2a3b19c32a1461141bde3f0
Alexandre Kaimer <alekaimer@gmail.com>
2021-03-01 22:31:29 -0300

    getServerSideProps to get information before the document is loaded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdows";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head'
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css'
-import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
-export default function Home() {
+
+export default function Home(props) {
+
+  console.log(props) // views props get from the client-side
+
   return (
     <div className={styles.container}>
       <Head>
@@ -35,3 +40,18 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => { // before the document is fully loaded (run in node server)
+  const { level, currentExperience, challengesCompleted } = context.req.cookies
+
+  // console.log(user) // views props get from the server-side
+
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted
+    }
+  }
+}
+
